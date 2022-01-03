f2bccb7d1fdf0eab217c8598eb90b9b8b10d9a32
M-W-Kirk <micah@kirkpatrickcore.com>
2022-01-02 20:19:02 -0500

    license updated

--- process_document_analysis/app.py
+++ process_document_analysis/app.py
@@ -1,3 +1,17 @@
 import os
 import json
 import boto3
+
+def get_kv_map(blocks):
+    key_map = {}
+    value_map = {}
+    block_map = {}
+    for block in blocks:
+        block_id = block['Id']
+        block_map[block_id] = block
+        if block['BlockType'] == "KEY_VALUE_SET":
+            if 'KEY' in block['EntityTypes']:
+                key_map[block_id] = block
+            else:
+                value_map[block_id] = block
+    return key_map, value_map, block_map
